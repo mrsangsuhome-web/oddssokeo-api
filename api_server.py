8e6d84e05c9a104518d507b203f913a171807d47
Tra <mrsangsuhome@gmail.com>
2026-05-18 11:05:49 +0700

    railway deployment fix

--- api_server.py
+++ api_server.py
@@ -116,11 +116,19 @@ threading.Thread(
     daemon=True
 ).start()
 
+import os
+
 if __name__ == "__main__":
+
+    port = int(
+        os.environ.get(
+            "PORT",
+            5001
+        )
+    )
 
     socketio.run(
         app,
         host="0.0.0.0",
-        port=5001,
-        debug=True
+        port=port
     )
